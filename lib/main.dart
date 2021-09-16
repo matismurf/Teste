be949330115af43483490390b794184f8c99492a
matismurf <viniciusaraujom@hotmail.com>
2021-09-16 10:32:34 -0300

    primeiro import

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Álcool ou Gasosalina?"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
